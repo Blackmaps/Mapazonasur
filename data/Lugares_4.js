ef571f8dbfd25408fdee5629a2d60243763712a8
msebash <msebaherrera@gmail.com>
2020-08-03 20:32:51 -0300

    Mapa con imagenes

---
+++ data/Lugares_4.js
@@ -0,0 +1,8 @@
+var json_Lugares_4 = {
+"type": "FeatureCollection",
+"name": "Lugares_4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": "1", "id": "1", "nombre": "Ayacucho 3009", "observ": "Buenos asados", "asado": "2020-10-10" }, "geometry": { "type": "Point", "coordinates": [ -58.36277549190158, -34.714425947139532 ] } }
+]
+}
